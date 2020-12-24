05a6e8f8b129c75d78ec10ab555a1674cc3e14f9
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-12-23 22:31:23 -0300

    test: ensure SignUpController calls emailsValidator with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -57,7 +57,7 @@ describe('SignUp Controller', () => {
     const httpRequest = {
       body: {
         name: 'any_name',
-        email: 'any_password',
+        email: 'any_email',
         passwordConfirmation: 'any_password'
       }
     }
@@ -71,7 +71,7 @@ describe('SignUp Controller', () => {
     const httpRequest = {
       body: {
         name: 'any_name',
-        email: 'any_password',
+        email: 'any_email',
         password: 'any_password'
       }
     }
@@ -87,7 +87,7 @@ describe('SignUp Controller', () => {
     const httpRequest = {
       body: {
         name: 'any_name',
-        email: 'invalid_password',
+        email: 'any_email',
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
@@ -96,5 +96,20 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+  })
+
+  test('should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
   })
 })

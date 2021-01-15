073cfaac5770231d62839f67f3810bf701debbf5
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-01-15 18:42:13 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,15 @@
+import { Encrypter } from '../../data/protocols/encrypter'
+import bcripty from 'bcrypt'
+
+export class BcryptAdapter implements Encrypter {
+  private readonly salt: number
+
+  constructor(salt: number) {
+    this.salt = salt
+  }
+
+  async encrypt(value: string): Promise<string> {
+    await bcripty.hash(value, this.salt)
+    return null
+  }
+}

4dfabadc825c9977ba686894a4a24855f5c95f53
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-01-22 19:43:44 -0300

    feat: add EmailValidation in ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -3,8 +3,19 @@ import { ValidationComposite } from '../../presentation/helpers/validators/valid
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { makeSignUpValidation } from './signup-validation'
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
+import { EmailValidator } from '../../presentation/protocols'
+import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
 
 jest.mock('../../presentation/helpers/validators/validation-composite')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
 
 describe('SignUpValidationFactory', () => {
   test('should call ValidatonComposite with all validations', () => {
@@ -17,6 +28,7 @@ describe('SignUpValidationFactory', () => {
     validations.push(
       new CompareFieldsValidation('password', 'passwordConfirmation')
     )
+    validations.push(new EmailValidation('email', makeEmailValidator()))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
---
+++ src/presentation/helpers/validators/email-validaton.spec.ts
@@ -0,0 +1,42 @@
+import { EmailValidator } from '../../protocols/email-validator'
+import { EmailValidation } from './email-validation'
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+interface SutTypes {
+  sut: EmailValidation
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator()
+  const sut = new EmailValidation('email', emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
+
+describe('Email Validation', () => {
+  test('should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    sut.validate({ email: 'any@email.com' })
+    expect(isValidSpy).toHaveBeenCalledWith('any@email.com')
+  })
+
+  test('return throw EmailValidator throws', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    expect(sut.validate).toThrow()
+  })
+})

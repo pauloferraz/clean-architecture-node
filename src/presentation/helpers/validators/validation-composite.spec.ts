7b2cd7a7f6db1f1f055849eed7d6b4c6f455ccf6
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-01-22 20:03:45 -0300

    test: add tests to validators

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,42 @@
+import { MissingParamError } from '../../errors'
+import { ValidationSpy } from '../../mocks/mock-validation'
+import { ValidationComposite } from './validation-composite'
+
+const field = 'any_field'
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationSpies: ValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpies = [new ValidationSpy(), new ValidationSpy()]
+  const sut = new ValidationComposite(validationSpies)
+  return {
+    sut,
+    validationSpies
+  }
+}
+
+describe('Validation Composite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut, validationSpies } = makeSut()
+    validationSpies[1].error = new MissingParamError(field)
+    const error = sut.validate({ [field]: 'any_field' })
+    expect(error).toEqual(validationSpies[1].error)
+  })
+
+  test('Should return the first error if more then one validation fails', () => {
+    const { sut, validationSpies } = makeSut()
+    validationSpies[0].error = new Error()
+    validationSpies[1].error = new MissingParamError(field)
+    const error = sut.validate({ [field]: 'any_field' })
+    expect(error).toEqual(validationSpies[0].error)
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ [field]: 'any_field' })
+    expect(error).toBeFalsy()
+  })
+})

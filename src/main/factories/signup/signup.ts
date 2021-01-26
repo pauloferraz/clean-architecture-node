43b1103fb98dab8475215f86914ca88d1e405ef5
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-01-26 08:54:03 -0300

    feat: add JwtAdapter

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,5 +1,5 @@
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
@@ -12,10 +12,7 @@ export const makeSignUpController = (): Controller => {
   const bcrypt = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcrypt, accountMongoRepository)
-  const signUpController = new SignUpController(
-    dbAddAccount,
-    makeSignUpValidation()
-  )
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }

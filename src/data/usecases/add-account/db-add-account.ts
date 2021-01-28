225fe86046cf9ddbf83d5c3098ccceb4715d5bd1
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-01-27 23:03:39 -0300

    feat: ensure LoadAccountByEmailRepository returns null by default

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,3 +1,4 @@
+import { LoadAccountByEmailRepository } from '../authentication/db-authentication-protocols'
 import {
   AddAccount,
   AddAccountModel,
@@ -9,14 +10,20 @@ import {
 export class DbAddAccount implements AddAccount {
   constructor(
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    const hashedPassword = await this.hasher.hash(accountData.password)
-    const account = await this.addAccountRepository.add(
-      Object.assign(accountData, { password: hashedPassword })
-    )
-    return account
+    const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(accountData.password)
+      const newAccount = await this.addAccountRepository.add(
+        Object.assign(accountData, { password: hashedPassword })
+      )
+      return newAccount
+    }
+
+    return null
   }
 }
--- src/main/factories/usecases/db-add-account-factory.ts
+++ src/main/factories/usecases/db-add-account-factory.ts
@@ -5,7 +5,7 @@ import { AddAccount } from '../../../domain/usecases/add-account'
 
 export const makeDbAddAccount = (): AddAccount => {
   const salt = 12
-  const bcrypt = new BcryptAdapter(salt)
+  const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
-  return new DbAddAccount(bcrypt, accountMongoRepository)
+  return new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
 }

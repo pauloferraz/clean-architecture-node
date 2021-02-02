593b47a9230d07cbc3e7335c7cae79c246ab433f
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-02-01 22:45:53 -0300

    feat: add Decrypter in JWT Adapter

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,11 +1,17 @@
 import { Encrypter } from '../../../data/protocols/cryptography/encrypter'
+import { Decrypter } from '../../../data/protocols/cryptography/decrypter'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor(private readonly secret: string) {}
 
   async encrypt(value: string): Promise<string> {
-    const accessToken = await jwt.sign({ id: value }, this.secret)
+    const accessToken = jwt.sign({ id: value }, this.secret)
     return accessToken
+  }
+
+  async decrypt(token: string): Promise<string> {
+    const value: any = jwt.verify(token, this.secret)
+    return value
   }
 }

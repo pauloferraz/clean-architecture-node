fc46274691a0b24d5d6955ce16b2ab4efc9b0b56
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-02-02 08:53:42 -0300

    feat: apply AuthMiddleware

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -17,6 +20,8 @@ describe('Login Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -34,5 +39,39 @@ describe('Login Routes', () => {
         })
         .expect(403)
     })
+  })
+
+  test('should return 204 on add survey with valid accessToken', async () => {
+    const res = await accountCollection.insertOne({
+      name: 'Paulo',
+      email: 'paulo@gmail.com',
+      password: '123',
+      role: 'admin'
+    })
+    const id = res.ops[0]._id
+    const accessToken = sign({ id }, env.jwtSecret)
+
+    await accountCollection.updateOne(
+      { _id: id },
+      {
+        $set: {
+          accessToken: accessToken
+        }
+      }
+    )
+
+    await request(app)
+      .post('/api/surveys')
+      .set('x-access-token', accessToken)
+      .send({
+        question: 'Question?',
+        answers: [
+          {
+            image: 'image_ul',
+            answer: 'Survey 001'
+          }
+        ]
+      })
+      .expect(204)
   })
 })

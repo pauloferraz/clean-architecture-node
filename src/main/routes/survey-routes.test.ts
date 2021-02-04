dbf04a2e6c3337bce7389b5a8843b8e049f799e7
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-02-04 19:57:22 -0300

    feat: add route get all surveys

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -8,7 +8,29 @@ import env from '../config/env'
 let surveyCollection: Collection
 let accountCollection: Collection
 
-describe('Login Routes', () => {
+const mockAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Rodrigo',
+    email: 'rodrigo.manguinho@gmail.com',
+    password: '123',
+    role: 'admin'
+  })
+  const id = res.ops[0]._id
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne(
+    {
+      _id: id
+    },
+    {
+      $set: {
+        accessToken
+      }
+    }
+  )
+  return accessToken
+}
+
+describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -25,53 +47,56 @@ describe('Login Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    test('should return 403 on add survey without accessToken', async () => {
+    test('Should return 403 on add survey without accessToken', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
-          question: 'Question?',
+          question: 'Question',
           answers: [
             {
-              image: 'image_ul',
-              answer: 'Survey 001'
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            },
+            {
+              answer: 'Answer 2'
             }
           ]
         })
         .expect(403)
     })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const accessToken = await mockAccessToken()
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
+        })
+        .expect(204)
+    })
   })
 
-  test('should return 204 on add survey with valid accessToken', async () => {
-    const res = await accountCollection.insertOne({
-      name: 'Paulo',
-      email: 'paulo@gmail.com',
-      password: '123',
-      role: 'admin'
+  describe('GET /surveys', () => {
+    test('Should return 403 on load surveys without accessToken', async () => {
+      await request(app).get('/api/surveys').expect(403)
     })
-    const id = res.ops[0]._id
-    const accessToken = sign({ id }, env.jwtSecret)
 
-    await accountCollection.updateOne(
-      { _id: id },
-      {
-        $set: {
-          accessToken: accessToken
-        }
-      }
-    )
-
-    await request(app)
-      .post('/api/surveys')
-      .set('x-access-token', accessToken)
-      .send({
-        question: 'Question?',
-        answers: [
-          {
-            image: 'image_ul',
-            answer: 'Survey 001'
-          }
-        ]
-      })
-      .expect(204)
+    test('Should return 204 on load surveys with valid accessToken', async () => {
+      const accessToken = await mockAccessToken()
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(204)
+    })
   })
 })
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
-import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory'
+import { adminAuth, auth } from '../middlewares/roles/roles'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }

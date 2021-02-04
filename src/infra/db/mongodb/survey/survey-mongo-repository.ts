ae95baf5fbafac84961f4e57ae71cb18624e15ba
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-02-04 18:53:24 -0300

    feat: add loadAll in SurveyMongoRepository

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,13 +1,21 @@
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
 import {
   AddSurveyModel,
   AddSurveyRepository
 } from '../../../../data/usecases/add-survey/db-survey-protocols'
+import { SurveyModel } from '../../../../domain/models/survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
-    const result = await surveyCollection.insertOne(surveyData)
-    return MongoHelper.map(result.ops[0])
+    const survey = await surveyCollection.insertOne(surveyData)
+    return MongoHelper.map(survey.ops[0])
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+    return surveys
   }
 }

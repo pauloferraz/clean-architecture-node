e94d2872c212fa70ae42f93681c2dedc7f8dc682
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-02-09 21:14:28 -0300

    feat: ensure save survey result route return 403 if invalid token

---
+++ src/main/factories/controllers/survey-result/save-survey-result-controller-factory.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@/presentation/protocols'
+import { SaveSurveyResultController } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller'
+import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
+import { makeDbLoadSurveyById } from '@/main/factories/usecases/survey/db-load-survey-by-id-factory'
+import { makeDbSaveSurveyResult } from '@/main/factories/usecases/survey-result/db-save-survey-result-factory'
+
+export const makeSaveSurveyResultController = (): Controller => {
+  const controller = new SaveSurveyResultController(
+    makeDbLoadSurveyById(),
+    makeDbSaveSurveyResult()
+  )
+  return makeLogControllerDecorator(controller)
+}
---
+++ src/main/routes/survey-results-routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
+import { auth } from '@/main/middlewares/roles/roles'
+import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result-controller-factory'
+
+export default (router: Router): void => {
+  router.put(
+    '/surveys/:surveyId/results',
+    auth,
+    adaptRoute(makeSaveSurveyResultController())
+  )
+}

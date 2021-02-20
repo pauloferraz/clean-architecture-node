86bb3e16b3f20ea25167cf0a1dbd21c21709fc88
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-02-20 14:59:59 -0300

    feat: add didAnswer in SurveyModel

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -7,6 +7,7 @@ import {
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
 import { ObjectId } from 'mongodb'
+import { QueryBuilder } from '../helpers'
 
 export class SurveyMongoRepository
   implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
@@ -16,9 +17,39 @@ export class SurveyMongoRepository
     return MongoHelper.map(survey.ops[0])
   }
 
-  async loadAll(): Promise<SurveyModel[]> {
+  async loadAll(accountId: string): Promise<SurveyModel[]> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
-    const surveys = await surveyCollection.find().toArray()
+    const query = new QueryBuilder()
+      .lookup({
+        from: 'surveyResults',
+        foreignField: 'surveyId',
+        localField: '_id',
+        as: 'result'
+      })
+      .project({
+        _id: 1,
+        question: 1,
+        answers: 1,
+        date: 1,
+        didAnswer: {
+          $gte: [
+            {
+              $size: {
+                $filter: {
+                  input: '$result',
+                  as: 'item',
+                  cond: {
+                    $eq: ['$$item.accountId', new ObjectId(accountId)]
+                  }
+                }
+              }
+            },
+            1
+          ]
+        }
+      })
+      .build()
+    const surveys = await surveyCollection.aggregate(query).toArray()
     return MongoHelper.mapCollection(surveys)
   }
 
--- src/presentation/test/mock-survey.ts
+++ src/presentation/test/mock-survey.ts
@@ -24,7 +24,7 @@ export const mockLoadSurveyById = (): LoadSurveyById => {
 
 export const mockLoadSurveys = (): LoadSurveys => {
   class LoadSurveysStub implements LoadSurveys {
-    async load(): Promise<SurveyModel[]> {
+    async load(accountId: string): Promise<SurveyModel[]> {
       return Promise.resolve(mockSurveyModels())
     }
   }

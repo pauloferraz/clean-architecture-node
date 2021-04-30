c2a1291d573188bb8814fa9de9ac099c6e2b068f
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-04-29 21:21:22 -0300

    test: use LoadCategoryByName in AddProductController

--- tests/presentation/controllers/add-product-controller.spec.ts
+++ tests/presentation/controllers/add-product-controller.spec.ts
@@ -1,6 +1,10 @@
 import { AddProductController } from '@/presentation/controllers'
 import { throwError } from '@/tests/domain/mocks'
-import { AddProductSpy, ValidationSpy } from '@/tests/presentation/mocks'
+import {
+  AddProductSpy,
+  LoadCategoryByNameSpy,
+  ValidationSpy
+} from '@/tests/presentation/mocks'
 import { badRequest, noContent, serverError } from '@/presentation/helpers'
 
 import faker from 'faker'
@@ -8,7 +12,7 @@ import faker from 'faker'
 const mockRequest = (): AddProductController.Request => ({
   name: faker.random.words(),
   description: faker.random.words(),
-  category: faker.random.words(),
+  category: 'any_category',
   price: faker.random.number(),
   qty_min: faker.random.number(),
   paymentType: faker.random.number(),
@@ -21,16 +25,23 @@ const mockRequest = (): AddProductController.Request => ({
 type SutTypes = {
   sut: AddProductController
   validationSpy: ValidationSpy
+  loadCategoryByNameSpy: LoadCategoryByNameSpy
   addProductSpy: AddProductSpy
 }
 
 const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy()
+  const loadCategoryByNameSpy = new LoadCategoryByNameSpy()
   const addProductSpy = new AddProductSpy()
-  const sut = new AddProductController(validationSpy, addProductSpy)
+  const sut = new AddProductController(
+    validationSpy,
+    loadCategoryByNameSpy,
+    addProductSpy
+  )
   return {
     sut,
     validationSpy,
+    loadCategoryByNameSpy,
     addProductSpy
   }
 }
@@ -51,21 +62,24 @@ describe('AddProduct Controller', () => {
   })
 
   test('Should call AddProduct with correct values', async () => {
-    const { sut, addProductSpy } = makeSut()
+    const { sut, addProductSpy, loadCategoryByNameSpy } = makeSut()
     const request = mockRequest()
+    loadCategoryByNameSpy.result = 'any_category'
     await sut.handle(request)
     expect(addProductSpy.params).toEqual(request)
   })
 
   test('Should return 500 if AddProduct throws', async () => {
-    const { sut, addProductSpy } = makeSut()
+    const { sut, addProductSpy, loadCategoryByNameSpy } = makeSut()
     jest.spyOn(addProductSpy, 'add').mockImplementationOnce(throwError)
+    loadCategoryByNameSpy.result = 'any_category'
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
   })
 
   test('Should return 204 on success', async () => {
-    const { sut } = makeSut()
+    const { sut, loadCategoryByNameSpy } = makeSut()
+    loadCategoryByNameSpy.result = 'any_category'
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(noContent())
   })

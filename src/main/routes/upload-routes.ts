d247b1f9a2e1c03e6fb7884dd161598523d1bf16
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-05-03 09:13:03 -0300

    feat: add max size files and types allowed

--- src/main/routes/upload-routes.ts
+++ src/main/routes/upload-routes.ts
@@ -2,13 +2,37 @@ import { Router, Request, Response } from 'express'
 import multer from 'multer'
 import { fileHandler } from '@/presentation/middlewares/file-handler'
 import { makeFileUploadController } from '@/main/factories/controllers'
+import path from 'path'
 
-const upload = multer()
+const MAX_SIZE_TWO_MEGABYTES = 2 * 1024 * 1024
+
+const upload = multer({
+  limits: {
+    fileSize: MAX_SIZE_TWO_MEGABYTES
+  },
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname)
+    if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+      return callback(new Error('Only images are allowed'))
+    }
+    callback(null, true)
+  }
+})
 
 export default (router: Router): void => {
   router.post(
     '/upload',
-    upload.array('files', 5),
+    function (req, res, next) {
+      upload.array('files', 5)(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+          return res.status(400).send({ error: err.message })
+        }
+        if (err) {
+          return res.status(400).send({ error: err.message })
+        }
+        next()
+      })
+    },
     fileHandler,
     async (req: Request, res: Response) => {
       const { statusCode, body } = await makeFileUploadController().handle({
